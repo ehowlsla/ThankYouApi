d81fb82b63fd85a7c1335e27155a7982b684536d
ehowlsla <rupy1014@gmail.com>
2013-11-27 21:43:08 +0900

    hi

---
+++ app/Contants/HttpContants.java
@@ -0,0 +1,8 @@
+package Contants;
+
+public class HttpContants {
+	public static int OK_200 = 200;
+	public static int FORBIDDEN_403 = 403;
+	public static int CONFLICT_409 = 409;
+	public static int EXPECTATION_FAILED_417 = 417;
+}
