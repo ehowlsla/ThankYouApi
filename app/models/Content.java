5a2354ebd3f6227bcf8ac3dce650d9822c8b1a24
leebokeum <leebokeum87@gmail.com>
2014-05-07 17:52:16 +0900

    ddfs

--- app/models/Content.java
+++ app/models/Content.java
@@ -92,10 +92,10 @@ public class Content extends Model{
 					return find.where().eq("status", 1).eq("user_id", user_id).lt("id", last_id).orderBy("id desc").setMaxRows(pSize).setFirstRow(0).findPagingList(pSize).getPage(0).getList();
 			} else {
 				if(last_id == 0)
-					return find.where().eq("status", 1).orderBy("id desc").findPagingList(pSize).getPage(0).getList();
+					return find.where().eq("status", 1).eq("openLevel", openLevel).orderBy("id desc").setMaxRows(pSize).setFirstRow(31).findPagingList(pSize).getPage(0).getList();
 				else
 //					return find.where().eq("status", 1).lt("id", last_id).eq("openLevel", openLevel).orderBy("id desc").findPagingList(pSize).getPage(0).getList();
-					return find.where().eq("status", 1).lt("id", last_id).findPagingList(pSize).getPage(0).getList();
+					return find.where().eq("status", 1).lt("id", last_id).eq("openLevel", openLevel).orderBy("id desc").setMaxRows(pSize).setFirstRow(31).findPagingList(pSize).getPage(0).getList();
 			}
 		} else {
 			if(openLevel == OpenLevel.LEVEL_ME) {
