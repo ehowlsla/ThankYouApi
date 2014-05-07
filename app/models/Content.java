d86e4d9a5dce30d27e78e96cdd003833876e6049
leebokeum <leebokeum87@gmail.com>
2014-05-07 17:08:57 +0900

    dsdf

--- app/models/Content.java
+++ app/models/Content.java
@@ -87,27 +87,27 @@ public class Content extends Model{
 		if(target_id == 0) {
 			if(openLevel == OpenLevel.LEVEL_ME) {
 				if(last_id == 0)
-					return find.where().eq("status", 1).eq("user_id", user_id).orderBy("id desc").setMaxRows(100).setFirstRow(0).findPagingList(pSize).getAsList();
+					return find.where().eq("status", 1).eq("user_id", user_id).orderBy("id desc").setMaxRows(pSize).setFirstRow(0).findPagingList(pSize).getPage(0).getList();
 				else
-					return find.where().eq("status", 1).eq("user_id", user_id).lt("id", last_id).orderBy("id desc").setMaxRows(100).setFirstRow(0).findPagingList(pSize).getAsList();
+					return find.where().eq("status", 1).eq("user_id", user_id).lt("id", last_id).orderBy("id desc").setMaxRows(pSize).setFirstRow(0).findPagingList(pSize).getPage(0).getList();
 			} else {
 				if(last_id == 0)
-					return find.where().eq("status", 1).eq("openLevel", openLevel).orderBy("id desc").setMaxRows(100).setFirstRow(0).findPagingList(pSize).getAsList();
+					return find.where().eq("status", 1).eq("openLevel", openLevel).orderBy("id desc").setMaxRows(pSize).setFirstRow(0).findPagingList(pSize).getPage(0).getList();
 				else
 //					return find.where().eq("status", 1).lt("id", last_id).eq("openLevel", openLevel).orderBy("id desc").findPagingList(pSize).getPage(0).getList();
-					return find.where().eq("status", 1).lt("id", last_id).eq("openLevel", openLevel).orderBy("id desc").setMaxRows(100).setFirstRow(0).findPagingList(pSize).getAsList();
+					return find.where().eq("status", 1).lt("id", last_id).eq("openLevel", openLevel).orderBy("id desc").setMaxRows(pSize).setFirstRow(0).findPagingList(pSize).getPage(0).getList();
 			}
 		} else {
 			if(openLevel == OpenLevel.LEVEL_ME) {
 				if(last_id == 0)
-					return find.where().eq("status", 1).eq("user_id", target_id).orderBy("id desc").findPagingList(pSize).getAsList();
+					return find.where().eq("status", 1).eq("user_id", target_id).orderBy("id desc").findPagingList(pSize).getPage(0).getList();
 				else
-					return find.where().eq("status", 1).eq("user_id", target_id).lt("id", last_id).orderBy("id desc").setMaxRows(100).setFirstRow(0).findPagingList(pSize).getAsList();
+					return find.where().eq("status", 1).eq("user_id", target_id).lt("id", last_id).orderBy("id desc").setMaxRows(pSize).setFirstRow(0).findPagingList(pSize).getPage(0).getList();
 			} else {
 				if(last_id == 0)
-					return find.where().eq("status", 1).eq("user_id", target_id).eq("openLevel", openLevel).orderBy("id desc").setMaxRows(100).setFirstRow(0).findPagingList(pSize).getAsList();
+					return find.where().eq("status", 1).eq("user_id", target_id).eq("openLevel", openLevel).orderBy("id desc").setMaxRows(pSize).setFirstRow(0).findPagingList(pSize).getPage(0).getList();
 				else
-					return find.where().eq("status", 1).eq("user_id", target_id).lt("id", last_id).eq("openLevel", openLevel).orderBy("id desc").setMaxRows(100).setFirstRow(0).findPagingList(pSize).getAsList();
+					return find.where().eq("status", 1).eq("user_id", target_id).lt("id", last_id).eq("openLevel", openLevel).orderBy("id desc").setMaxRows(pSize).setFirstRow(0).findPagingList(pSize).getPage(0).getList();
 			}
 		} 
 	}
