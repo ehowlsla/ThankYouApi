fcea3db8bb78617386b8ff63354496bb6720f991
이복음 <leebokeum@ibog-eum-ui-MacBook-Pro.local>
2016-11-12 13:57:01 +0900

    aa

--- app/resModles/ResUser.java
+++ app/resModles/ResUser.java
@@ -8,35 +8,29 @@ public class ResUser {
  
  
 	public long id;
-	public String nickname = "";
-	public String udid = ""; 
+	public String nickname;
+	public String udid; 
 	public int status;
-	public String memo = "";
+	public String memo;
 	public int gender;
-	public String city = "";
+	public String city;
 	public Date create_date;
-	public String image_url1 = "";
-	public String image_url2 = "";
-	public String image_url3= "";
-	public String image_url4= "";
-	public String birth= "";
-	public String job= "";
-	public String token_key= "";
-	public String app_version= "";
-	public String os_version= "";
-	public String phone= "";
-	public String device_id = "";
-	public String email = "";
-	public String password = "";
+	public String image_url1;
+	public String image_url2;
+	public String image_url3;
+	public String image_url4;
+	public String birth;
+	public String job;
+	public String token_key;
+	public String app_version;
+	public String os_version;
+	public String phone;
+	public String device_id;
+	public String email;
+	public String password;
 
 	public ResUser(User obj) {
 		// TODO Auto-generated constructor stub
-		
-		
-		if(obj.image_url1 == null){
-			this.id = obj.id;
-		}
-
 		if(obj != null) {
 			this.id = obj.id;
 			this.nickname = obj.nickname;
@@ -52,13 +46,13 @@ public class ResUser {
 			this.status = obj.status;
 			this.gender = obj.gender;
 			this.create_date = obj.create_date;
-			this.token_key = "";
-			this.app_version = "";
-			this.os_version = "";
-			this.phone = "";
-			this.device_id = "";
-			this.email = "";
-			this.password = "";
+			this.token_key = obj.token_key;
+			this.app_version = obj.app_version;
+			this.os_version = obj.os_version;
+			this.phone = obj.phone;
+			this.device_id = obj.device_id;
+			this.email = obj.email;
+			this.password = obj.password;
 		}
 	}
 }
