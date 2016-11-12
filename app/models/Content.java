f49152a7062ae4bede197f5036146a4e0593a67c
이복음 <leebokeum@ibog-eum-ui-MacBook-Pro.local>
2016-11-12 14:45:58 +0900

    ad

--- app/models/Content.java
+++ app/models/Content.java
@@ -61,7 +61,8 @@ public class Content extends Model{
 	
 	public static Finder<Long,Content> find = new Finder<Long,Content>(Long.class, Content.class); 
 
-	@ManyToOne(optional = true, fetch=FetchType.EAGER)
+	@ManyToOne(optional = true)
+	@JoinColumn(name = "user_id")
 	public User user;
 	
 	private static final int pSize = 30;
